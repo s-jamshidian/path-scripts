8a813baeb8904042ce707f14a520fae52f0b46b5
Sajjad Jamshidian <sjamshidian@outlook.com>
2025-04-30 20:39:43 +0330

    'compile_cpython.sh' -- add cli argument parser and some minor improvements

--- compile_cpython.sh
+++ compile_cpython.sh
@@ -1,17 +1,63 @@
 #!/bin/sh
 #
-# Compile CPython from source on Debian or Debian derivative operating systems.
-# Arguments:
-#    - Download link of CPython.
-#    - MD5 hexadecimal digest of CPythons' compressed tarball.
-#    - Existing Path to install CPython in.
-###############################################################################
+# Compile CPython from source on Debian-based systems.
 
 set -e -u
 
-URL="$1"
-DIGEST="$2"
-INSTALL_PATH="$3"
+PROG="${0##*/}"
+N_ARGS=2
+
+# Exit codes:
+E_OK=0
+E_USAGE=79
+
+# File descriptors:
+STDOUT=1
+STDERR=2
+
+# Write usage message to the specified file descriptor and exit the shell.
+# Globals:
+#   PROG
+#   STDOUT
+#   E_USAGE
+# Arguments:
+#   - File descriptor to write the usage message to. (Default: STDOUT)
+#   - Exit the shell with this code. (Default: E_USAGE)
+usage() {
+    cat >&"${1:-"${STDOUT}"}" <<__END__
+${PROG} - Compile CPython from source on Debian-based systems.
+
+Usage:
+    ${PROG} [OPTIONS...] <URL> <MD5_CHECKSUM>
+
+Arguments:
+    <URL>           Download URL for CPython source tarball.
+    <MD5_CHECKSUM>  MD5 checksum of the CPython source tarball.
+
+Options:
+    -d <DIR>   Install directory.
+    -h         Show this help and exit.
+
+Exit Status:
+    0   Success.
+    79  Invalid option(s) or missing argument(s).
+__END__
+
+    exit "${2:-"${E_OK}"}"
+}
+
+install_path=''
+while getopts ':hd:' option; do
+    case "${option}" in
+        d) install_path="${OPTARG}" ;;
+        h) usage ;;
+        *) usage "${STDERR}" "${E_USAGE}" ;;
+    esac
+done
+shift $((OPTIND - 1))
+[ "$#" -ne "${N_ARGS}" ] && usage "${STDERR}" "${E_USAGE}"
+url="$1"
+digest="$2"
 
 # Essential dependency packages.
 sudo apt-get update -y
@@ -40,39 +86,32 @@ sudo apt-get install -y \
 
 if ! tempdir="$(mktemp -d 2>/dev/null)"; then
     tempdir="/tmp/$$"
-    mkdir "${tempdir}"
+    mkdir -- "${tempdir}"
 fi
+trap 'rm -r -f -- "${tempdir}"' EXIT
+cd -- "${tempdir}"
 
-trap 'rm -rf -- "${tempdir}"' EXIT
+filename="${url##*/}"
+filename="${filename%%[?#]*}" # Strip URL parameters/fragments
 
-cd "${tempdir}"
-
-filename="${URL##*/}"
-filename="${filename%%[?#]*}" # Remove parameters and fragment if exists
-
-wget -nv -O "${filename}" "${URL}"
-echo "${DIGEST} *${filename}" | md5sum -c
+wget -nv --show-progress -O "${filename}" -- "${url}"
+echo "${digest} *${filename}" | md5sum -c
 
 tar -x -a -f "${filename}"
-cd "$(tar -t -f "${filename}" | head -n 1 | cut -d / -f 1)"
+extracted_dir="$(tar -t -f "${filename}" | head -n 1 | cut -d / -f 1)"
+cd -- "${extracted_dir}"
 
 # Optimization flags
 export LDFLAGS="-flto"
-export CFLAGS="\
--O3 \
--flto \
--pipe \
--mtune=native \
--march=native \
-"
+export CFLAGS="-O3 -flto -pipe -mtune=native -march=native"
 
 ./configure \
-    --prefix="${INSTALL_PATH}" \
+    ${install_path:+--prefix="${install_path}"} \
     --enable-optimizations \
     --with-lto=full \
     --with-computed-gotos
 
-make -j "$(nproc || echo 1)"
+make -j "$(nproc 2>/dev/null || echo 1)"
 make altinstall
 
 echo "All Done!"
