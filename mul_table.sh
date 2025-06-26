6d3602c2062be54620aa19f8546d207b1509ee28
Sajjad Jamshidian <sjamshidian@outlook.com>
2025-06-26 19:11:29 +0330

    Slightly refactor

--- mul_table.sh
+++ mul_table.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 set -eu
+
 ##########################################
 # Safe to experiment with these constants.
 ##########################################
@@ -15,33 +17,34 @@ readonly ALT_COLOR_BY='column' # Valid values: 'row' or 'column'.
 readonly CSI='\033['
 readonly RESET="${CSI}0m"
 readonly BOLD="${CSI}1m"
-readonly B_RED="${CSI}91m"
-readonly B_CYAN="${CSI}96m"
-readonly B_WHITE="${CSI}97m"
+readonly HEADER_COLOR="${CSI}97m"
+readonly COLOR1="${CSI}91m"
+readonly COLOR2="${CSI}96m"
 ##########################################
 
 readonly MAX_PRODUCT=$((UPPER_BOUND * UPPER_BOUND))
-readonly COL_OFFSET=$((${#UPPER_BOUND}))
+readonly COL_OFFSET="${#UPPER_BOUND}"
 readonly COL_WIDTH=$((${#MAX_PRODUCT} + 1))
 
 factors="$(seq "${LOWER_BOUND}" "${UPPER_BOUND}")"
-printf '%b' "${BOLD}" "${B_WHITE}" # Use bold font throughout the table
+printf '%b' "${BOLD}" "${HEADER_COLOR}" # Use bold font throughout the table
 printf '%*s' "${COL_OFFSET}" ''
 printf "%${COL_WIDTH}d" ${factors}
 
 for row in ${factors}; do
-    printf '\n%b%*d' "${B_WHITE}" "${COL_OFFSET}" "${row}"
+    printf '%b\n%*d' "${HEADER_COLOR}" "${COL_OFFSET}" "${row}"
     for column in ${factors}; do
+        # Proxy variable intentionally left unquoted.
         if [ $((${ALT_COLOR_BY} & 1)) -eq 1 ]; then
-            color="${B_CYAN}"
+            color="${COLOR1}"
         else
-            color="${B_RED}"
+            color="${COLOR2}"
         fi
         if [ "${SKIP_REDUNDANT}" = 'true' ] && [ "${column}" -lt "${row}" ]; then
             printf '%*s' "${COL_WIDTH}" ''
         else
-            printf "%b%*d" "${color}" $((COL_WIDTH)) $((row * column))
+            printf "%b%*d" "${color}" "${COL_WIDTH}" $((row * column))
         fi
     done
 done
-printf "${RESET}\n"
+printf "%b\n" "${RESET}"
